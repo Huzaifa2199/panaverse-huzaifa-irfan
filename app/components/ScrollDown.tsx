e0a4935b4b6e610d0a4a5c5f82c4358f4a565f64
Huzaifa <huzaifa.m2199@gmail.com>
2023-03-29 07:22:43 +0500

    animation added

--- app/components/ScrollDown.tsx
+++ app/components/ScrollDown.tsx
@@ -6,7 +6,7 @@ export const ScrollDown = () => {
     return (
         <div className='flex justify-end max-lg:hidden'>
             <div className='fixed z-10 top-[39vh]'>
-                <div className='pr-7 opacity-[85%]'>
+                <div className='bounce animate-bounce pr-7 opacity-[85%]'>
                     <Scroller></Scroller>
                 </div>
             </div>
